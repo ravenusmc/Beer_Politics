d9c17015e927266b6381acec1813e9d6134327f8
ravenusmc <mcuddy77@gmail.com>
2018-04-29 14:32:13 -0400

    added code to build first graph

--- graph.py
+++ graph.py
@@ -13,8 +13,69 @@ import pandas as pd
 
 class Graph():
 
-    def __init__(self):
-        self.data = pd.read_csv('./data/beer_politics.csv')
+    # def __init__(self):
+    #     self.data = pd.read_csv('./data/beer_politics.csv')
 
-    def test(self):
-        print()
+    def build_clinton_to_breweries_graph(self):
+        #Creating the file where the html will be built to
+        output_file('clinton_breweries.html')
+        #loading the csv to the file 
+        file = './data/beer_politics.csv'
+        #Reading and then storing the csv file as a variable 
+        data = pd.read_csv(file)
+        #Turning the data into a columndatasource
+        clinton_breweries = ColumnDataSource(data)
+
+        plot =  figure(x_axis_label='Percentage who voted for clinton', y_axis_label='Number of breweries',
+            plot_width=600, plot_height=500, tools='pan,wheel_zoom,box_zoom,reset,hover,save',
+            title='number of breweries versus support for Clinton')
+
+        plot.circle(x='clinton_per', y='breweries', source=clinton_breweries,
+            size=15)
+
+        show(plot)
+
+test = Graph()
+test.build_clinton_to_breweries_graph()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
