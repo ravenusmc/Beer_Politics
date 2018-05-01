ea3a4046bbe8ba0c986f890e097b2059e7780241
ravenusmc <mcuddy77@gmail.com>
2018-05-01 15:25:21 -0400

    added code to see regions on graph

--- graph.py
+++ graph.py
@@ -94,15 +94,23 @@ class Graph():
         google_breweries = ColumnDataSource(data)
 
         #This line will be what sets up a color code between which states voted for clinton or trump.
-        color_mapper = CategoricalColorMapper(factors=['Trump', 'Clinton'],
-            palette=['red', 'blue']) 
+        # color_mapper = CategoricalColorMapper(factors=['Trump', 'Clinton'],
+        #     palette=['red', 'blue']) 
+        
+        #Region color_mapper
+        color_mapper = CategoricalColorMapper(factors=['plains', 'atlantic','north east', 'west', 'south', 'rust', 'midwest'],
+            palette=['red', 'blue', 'black', 'orange', 'green', 'yellow', 'purple']) 
 
         plot = figure(x_axis_label='Percentage who voted for Clinton', y_axis_label='Google Trends Percentage of Searching for Craft Brewery',
         plot_width=600, plot_height=500, tools='pan,wheel_zoom,box_zoom,reset,hover,save',
         title='Google Percentage (Election) versus Support for Clinton')
 
+        # plot.circle(x='clinton_per', y='craft_beer_search', source=google_breweries,
+        # size=15, color=dict(field='who_won', transform=color_mapper))
+
+        #Region plot.cirlce code
         plot.circle(x='clinton_per', y='craft_beer_search', source=google_breweries,
-        size=15, color=dict(field='who_won', transform=color_mapper))
+            size=15, color=dict(field='region', transform=color_mapper))
 
         #setting up the hover tools
         hover = plot.select_one(HoverTool)
